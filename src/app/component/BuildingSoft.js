35e4133d514e0c3f6d5bdfa21707f4da9285b38f
difmo <difmotech@gmail.com>
2024-12-28 21:51:07 +0530

    save changes

--- src/app/component/BuildingSoft.js
+++ src/app/component/BuildingSoft.js
@@ -1,13 +1,13 @@
 import React from "react";
-import see from "../assets/yes1.png";
+import see from "../assets/tech.png";
 import Image from "next/image";
 const BuildingSoft = () => {
   return (
-    <div className="mx-4 sm:mx-8 lg:mx-16 mt-20 my-12">
+    <div className="mx-4 my-12 mt-20 sm:mx-8 lg:mx-16">
       {/* Grid Container for mobile responsive layout */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
         {/* Text Section */}
-        <div className=" rounded-lg">
+        <div className="rounded-lg ">
           <h2 className="md:text-3xl text-4xl font-lilita font-extrabold text-transparent bg-clip-text bg-gradient-to-r  from-deep-blue to-[#EA130C]  mb-6">
             Building Software for World Changers
           </h2>
@@ -23,7 +23,7 @@ const BuildingSoft = () => {
           <Image
             src={see} // Image from public folder
             alt="About Us"
-            className="w-full h-50 transition-all duration-500 ease-in-out transform hover:scale-105 hover:translate-x-4 hover:translate-y-4 hover:rotate-3"
+            className="w-full h-64 transition-all duration-500 ease-in-out transform hover:scale-105 hover:translate-x-4 hover:translate-y-4 "
           />
         </div>
       </div>
