cac247de50475691d28b5f672033c5db6b3eee94
Difmo Technologies <difmotech@gmail.com>
2025-02-27 13:59:31 +0530

    Update Header.js

--- src/app/component/Header.js
+++ src/app/component/Header.js
@@ -58,7 +58,7 @@ const Header = () => {
 
   return (
     <header
-      className={`fixed top-0 left-0 w-full z-60 px-20 transition-all duration-300 ${
+      className={`fixed top-0 left-0 w-full z-60 md:px-20 lg:px-20 transition-all duration-300 ${
         scrolling ? "bg-white " : "bg-transparent"
       }`}
     >
