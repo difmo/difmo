569ab4cb892c97cfcb2c4676324fa79f0bbdc4d3
Difmo Technologies <difmotech@gmail.com>
2025-03-03 19:22:36 +0000

    fix

--- src/app/component/landingpage/LandingPage.js
+++ src/app/component/landingpage/LandingPage.js
@@ -137,14 +137,8 @@ const LandingPage = () => {
       {/* Modal Section */}
       {isModalOpen && (
         <div className="fixed mt-16 inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div
-            className="modal-content bg-white rounded-lg shadow-lg p-6 max-w-lg relative"
-            ref={modalRef}
-          >
-          
           
             <RequestForm  isOpen={isModalOpen} onClose={handleCloseModal}/>
-          </div>
         </div>
       )}
     </div>
