1fe8598f461d5cc7627f57d150f522816de547eb
Difmo Technologies <difmotech@gmail.com>
2025-05-17 18:02:20 +0530

    Update Header.js

--- src/app/component/Header.js
+++ src/app/component/Header.js
@@ -45,7 +45,7 @@ const Header = () => {
           <Image
             src={logo}
             alt="Difmo Logo"
-            className="w-auto  md:h-10 bg-cover"
+            className="w-auto  bg-cover"
           />
         </Link>
 
