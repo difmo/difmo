22c467f6e84ca51d0c5615fcb70b80e1f0478e40
7844Pritam <pritamsharma7844@gmail.com>
2025-05-22 19:11:03 +0530

    save

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -10,7 +10,7 @@ const Contact = () => {
       {/* <div className="py-24"> */}
       <div className="bg-slate-50 py-8 pt-24 md:py-24 px-2 md:px-24">
         <h1 className="text-4xl py-3 font-bold text-gray-800 text-center mb-8">
-          Contact fUs
+          Contact Us
         </h1>
         <div className="text-lg text-gray-700">
           <p className="mb-4 text-center w-full lg:px-44">
